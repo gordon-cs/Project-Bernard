fa09c1fd89e0472694f71d2fadcfe8c1ee1a557d
Adam Bartholomew <adam.bartholomew@gordon.edu>
2016-07-05 17:02:59 -0400

    added buttons to each table item within the roster

--- app/routes/my-activities.js
+++ app/routes/my-activities.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model() {
+	model(param) {
+		var model = {
+
+		}
+
+		Ember.$.ajax({
+			type: 'GET',
+			url: 'https://ccttrain.gordon.edu/api/students/50154997/memberships',
+			async: false,
+			success: function(data) {
+				model.memberships = data;
+			},
+			error: function(errorThrown) {
+				console.log(errorThrown);
+			}
+		})
 		return Ember.$.getJSON('https://ccttrain.gordon.edu/api/students/50154997/memberships');
 	}
 });
