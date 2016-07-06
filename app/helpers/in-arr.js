122b04d4a5309abe5930a0caea12bc5054baaf7a
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2016-07-06 11:36:21 -0400

    Responsive styling changes

--- app/helpers/in-arr.js
+++ app/helpers/in-arr.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
 
-export function notInArr(params/*, hash*/) {
-
+export function inArr(params/*, hash*/) {
   var objectToFind = params[0];
   var restOfArray = params[1];
   console.log(restOfArray.indexOf(objectToFind) < 0);
-  return restOfArray.indexOf(objectToFind) < 0;
+  return restOfArray.indexOf(objectToFind) >= 0;
 }
 
-export default Ember.Helper.helper(notInArr);
+export default Ember.Helper.helper(inArr);
--- tests/unit/helpers/in-arr-test.js
+++ tests/unit/helpers/in-arr-test.js
@@ -1,10 +1,10 @@
-import { notInArr } from 'test-app/helpers/not-in-arr';
+import { inArr } from 'test-app/helpers/in-arr';
 import { module, test } from 'qunit';
 
-module('Unit | Helper | not in arr');
+module('Unit | Helper | in arr');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = notInArr([42]);
+  let result = inArr([42]);
   assert.ok(result);
 });
