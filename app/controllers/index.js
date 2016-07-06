ba0014b7a9e72c896a6e7808632089ef7504d256
jameskempf <james.kempf@gordon.edu>
2016-07-06 12:55:30 -0400

    add search bar

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -17,7 +17,19 @@ export default Ember.Controller.extend({
                 }
             });
             this.set('model.activities', activities);
+            this.set('model.activitiesShown', activities);
             this.set('model.currentSession', session);
+        },
+        filterActivities: function() {
+            var searchValue = this.get("searchValue");
+            var newList = [];
+            var oldList = this.get("model.activities");
+            for (var i = 0; i < oldList.length; i ++) {
+                if (oldList[i].ActivityDescription.indexOf(searchValue) !== -1) {
+                    newList.push(oldList[i]);
+                }
+            }
+            this.set("model.activitiesShown", newList);
         }
     }
 });
