b56b9c14c5d25c3d0f6908e6d4c4ba1274e1b506
jameskempf <james.kempf@gordon.edu>
2016-07-11 12:29:44 -0400

    install

---
+++ app/controllers/all-activities.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        selectSession: function(session) {
+            var activities = null;
+            Ember.$.ajax({
+                type: "GET",
+                url: "http://ccttrain.gordon.edu/KJzKJ6FOKx/api/sessions/" + session.SessionCode.trim() + "/activities",
+                async: false,
+                success: function(data) {
+                    activities = data;
+                    console.log(data);
+                },
+                error: function(errorThrown) {
+                    console.log(errorThrown);
+                }
+            });
+            this.set('model.activities', activities);
+            this.set('model.activitiesShown', activities);
+            this.set('model.currentSession', session);
+        },
+        filterActivities: function() {
+            var searchValue = this.get("searchValue");
+            var newList = [];
+            var oldList = this.get("model.activities");
+            for (var i = 0; i < oldList.length; i ++) {
+                if (oldList[i].ActivityDescription.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1) {
+                    newList.push(oldList[i]);
+                }
+            }
+            this.set("model.activitiesShown", newList);
+        }
+    }
+});
---
+++ app/routes/all-activities.js
@@ -0,0 +1,54 @@
+import Ember from "ember";
+
+export default Ember.Route.extend({
+    model() {
+        var model = {
+            "activities": [],
+            "activitesShown": [],
+            "sessions": [],
+            "currentSession": null
+        };
+        // Get Sessions Data
+        Ember.$.ajax({
+            type: "GET",
+            url: "http://ccttrain.gordon.edu/KJzKJ6FOKx/api/sessions",
+            async: false,
+            success: function(data) {
+                for (var i = data.length - 1; i >= 0; i --) {
+                    model.sessions.push(data[i]);
+                }
+                console.log(model.sessions);
+            },
+            error: function(errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+        // Get Current Session
+        Ember.$.ajax({
+            type: "GET",
+            url: "http://ccttrain.gordon.edu/KJzKJ6FOKx/api/sessions/current",
+            async: false,
+            success: function(data) {
+                model.currentSession = data;
+            },
+            error: function(errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+        // Get Activities in Session
+        Ember.$.ajax({
+            type: "GET",
+            url: "http://ccttrain.gordon.edu/KJzKJ6FOKx/api/sessions/" + model.currentSession.SessionCode + "/activities",
+            async: false,
+            success: function(data) {
+                model.activities = data;
+                model.activitiesShown = data;
+            },
+            error: function(errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+        console.log(model);
+        return model;
+    }
+});
