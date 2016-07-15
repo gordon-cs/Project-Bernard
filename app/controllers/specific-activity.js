ccfaeafcaeab4a81655e947457160242a75eaf91
jameskempf <james.kempf@gordon.edu>
2016-07-15 16:28:25 -0400

    confirmation

--- app/controllers/specific-activity.js
+++ app/controllers/specific-activity.js
@@ -76,32 +76,30 @@ export default Ember.Controller.extend({
         },
         // Approve specified membership request
         approveRequest(request) {
-            var confirmation = confirm("Accept this request?");
-            if(confirmation) {
-                // var success = false;
-                // this.get("session").authorize("authorizer:oauth2", (headerName, headerValue) => {
-                //     Ember.$.ajax({
-                //         type: "POST",
-                //         url: "https://gordon360api.gordon.edu/api/requests/" + request.RequestID + "/approve",
-                //         contentType: "application/json",
-                //         async: false,
-                //         headers: {
-        		// 			"Authorization": headerValue
-        		// 		},
-                //         success: function(data) {
-                //             success = true;
-                //         }
-                //     });
-                // });
-                // if (success) {
+            if(confirm("Accept this request?")) {
+                var success = false;
+                this.get("session").authorize("authorizer:oauth2", (headerName, headerValue) => {
+                    Ember.$.ajax({
+                        type: "POST",
+                        url: "https://gordon360api.gordon.edu/api/requests/" + request.RequestID + "/approve",
+                        contentType: "application/json",
+                        async: false,
+                        headers: {
+        					"Authorization": headerValue
+        				},
+                        success: function(data) {
+                            success = true;
+                        }
+                    });
+                });
+                if (success) {
                     window.location.reload(true);
-                // }
+                }
             }
         },
         // Deny specified membership request
         denyRequest(request) {
-            var confirmation = confirm("Deny this request?");
-            if (confirmation) {
+            if (confirm("Deny this request?")) {
                 var success = false;
                 this.get("session").authorize("authorizer:oauth2", (headerName, headerValue) => {
                     Ember.$.ajax({
