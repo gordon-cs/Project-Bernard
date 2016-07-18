4ee7e064540bac3400761fe27c097cdf9c2b2b21
jameskempf <james.kempf@gordon.edu>
2016-07-18 11:53:01 -0400

    finish request notifications

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -10,54 +10,54 @@ export default Ember.Controller.extend({
             this.get('session').invalidate();
         },
         getRequests() {
-            this.get('session').authorize('authorizer:oauth2', (headerName, headerValue) => {
-                var memberships = [];
-                var leaderMemberships = [];
-                var requests = [];
-                Ember.$.ajax({
-                    type: "GET",
-                    url: "https://gordon360api.gordon.edu/api/memberships/student/" + this.get('session.data.authenticated.token_data.id'),
-                    async: false,
-                    headers: {
-    					"Authorization": headerValue
-    				},
-                    success: function(data) {
-                        memberships = data;
-                    },
-                    error: function(errorThrown) {
-                        console.log(errorThrown);
-                    }
-                });
-                for (var i = 0; i < memberships.length; i ++) {
-                    if (isLeader(memberships[i].Participation)) {
-                        leaderMemberships.push(memberships[i]);
-                    }
-                }
-                for (var i = 0; i < leaderMemberships.length; i ++) {
-                    console.log("https://gordon360api.gordon.edu/api/requests/" + leaderMemberships[i].ActivityCode);
+            if (this.requests === null) {
+                console.log(new Date());
+                this.get('session').authorize('authorizer:oauth2', (headerName, headerValue) => {
+                    var memberships = [];
+                    var leaderMemberships = [];
+                    var requests = [];
                     Ember.$.ajax({
                         type: "GET",
-                        url: "https://gordon360api.gordon.edu/api/requests/" + leaderMemberships[i].ActivityCode,
+                        url: "https://gordon360api.gordon.edu/api/memberships/student/" + this.get('session.data.authenticated.token_data.id'),
+                        async: false,
                         headers: {
         					"Authorization": headerValue
         				},
                         success: function(data) {
-                            for (var j = 0; j < data.length; j ++) {
-                                if (data[j].SessionCode === leaderMemberships[i].SessionCode) {
-                                    requests.push(data[i]);
-                                }
-                            }
+                            memberships = data;
+                        },
+                        error: function(errorThrown) {
+                            console.log(errorThrown);
                         }
                     });
-                }
-                if (requests.length > 0) {
-                    this.set("notificationsPresent", true);
-                }
-                console.log(memberships);
-                console.log(leaderMemberships);
-                console.log(requests);
-                this.set("requests", requests);
-            });
+                    for (var i = 0; i < memberships.length; i ++) {
+                        if (isLeader(memberships[i].Participation)) {
+                            leaderMemberships.push(memberships[i]);
+                        }
+                    }
+                    for (var i = 0; i < leaderMemberships.length; i ++) {
+                        Ember.$.ajax({
+                            type: "GET",
+                            url: "https://gordon360api.gordon.edu/api/requests/activity/" + leaderMemberships[i].ActivityCode,
+                            async: false,
+                            headers: {
+            					"Authorization": headerValue
+            				},
+                            success: function(data) {
+                                for (var j = 0; j < data.length; j ++) {
+                                    if (data[j].SessionCode === leaderMemberships[i].SessionCode) {
+                                        requests.push(data[j]);
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    if (requests.length > 0) {
+                        this.set("notificationsPresent", true);
+                    }
+                    this.set("requests", requests);
+                });
+            }
         }
     }
 });
