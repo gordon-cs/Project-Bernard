929a862bc6c64fcd7221dac64787d70c30e29802
Adam Bartholomew <adam.bartholomew@gordon.edu>
2016-07-22 16:32:00 -0400

    responsive image styling for specific activity

--- app/routes/specific-activity.js
+++ app/routes/specific-activity.js
@@ -11,6 +11,8 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
         let activity = getSync("/activities/" + param.ActivityCode, this).data;
         let session = getSync("/sessions/" + param.SessionCode, this).data;
         let leading = false;
+        let hasLeaders = false;
+        let hasSupervisors = false;
 
         // If the logged in user has admin rights give them to him
         let godMode = this.get('session.data.authenticated.token_data.college_role') === "god";
@@ -24,21 +26,24 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
             if (supervisors[i].SessionCode.trim() !== param.SessionCode) {
                 console.log("REMOVE");
                 supervisors.splice(i --, 1);
+                hasSupervisors = true;
             }
             else if (supervisors[i].IDNumber == this.get("session.data.authenticated.token_data.id")) {
                 leading = true;
+                hasSupervisors = true;
             }
         }
 
         // Get leaders for session and check if user is a leader
         let leaders = getSync("/memberships/activity/" + param.ActivityCode + "/leaders", this).data;
-        console.log(leaders);
         for (var i = 0; i < leaders.length; i ++) {
             if (leaders[i].SessionCode !== param.SessionCode) {
                 leaders.splice(i --, 1);
+                hasLeaders = true;
             }
             else if (leaders[i].IDNumber == IDNumber) {
                 leading = true;
+                hasLeaders = true;
             }
         }
 
@@ -116,6 +121,8 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
             "allMyMembershipIDs": allMyMembershipIDs,
             // Misc
             "requests": sortJsonArray(requests, "LastName"),
+            "hasLeaders": hasLeaders,
+            "hasSupervisors": hasSupervisors,
             "requestsFilled": (requests.length > 0)
         };
     }
