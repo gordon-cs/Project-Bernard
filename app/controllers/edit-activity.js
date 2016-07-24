38d5ce366d32c044eca0169b8fa04def0e5ceda3
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2016-07-23 20:25:02 -0400

    Add image upload

--- app/controllers/edit-activity.js
+++ app/controllers/edit-activity.js
@@ -1,5 +1,36 @@
 import Ember from "ember";
 import putSync from "gordon360/utils/put-sync";
+import postFileSync from "gordon360/utils/post-file-sync";
+
+// Validate the selected image
+function validateImage(file){
+
+  let validFileExtensions = ['png','jpg','jpeg','bmp','gif'];
+  let result = {
+    isValid : true,
+    validationMessage : ''
+  };
+
+  if(file == undefined) {
+    result.isValid = false;
+    result.validationMessage = "No image file was selected.";
+    return result;
+  }
+
+  let fileExtentsion = file.name.split('.').pop() || '';
+  // The extension is not in the list of valid extensions
+  if(validFileExtensions.indexOf(fileExtentsion) === -1) {
+    result.isValid = false;
+    result.validationMessage = 'Unacceptable file extension.';
+  }
+  // File is greater than 100KB
+  if(file.size > 100000) {
+    result.isValid = false;
+    result.validationMessage = 'Unacceptable file size.';
+  }
+
+  return result;
+}
 
 export default Ember.Controller.extend({
     session: Ember.inject.service("session"),
@@ -19,6 +50,22 @@ export default Ember.Controller.extend({
             if (imageUrl == null || imageUrl == "") {
                 imageUrl = this.get("model.activity.ActivityImage");
             }
+
+            /* Image Upload */
+            let image = Ember.$("#file")[0].files[0];
+            let imageValidation = validateImage(image);
+            if (imageValidation.isValid) {
+              console.log(image);
+              let imageData = new FormData();
+              imageData.append(image.name, image);
+              let activityCode = this.get("model.activity.ActivityCode");
+              let response = postFileSync('/activities/'+activityCode+'/image', imageData, this);
+            }
+            else{
+              console.log(imageValidation.validationMessage + '\nNo image will be uploaded.');
+            }
+
+
             let data = {
                 "ACT_CDE": this.get("model.activity.ActivityCode"),
                 "ACT_DESC": this.get("model.activity.ActivityDescription"),
