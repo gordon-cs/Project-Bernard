af505db0a1e3958b0830df4854d3c45d4b9a36b9
jameskempf <james.kempf@gordon.edu>
2016-07-25 14:49:21 -0400

    fix

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,11 +2,6 @@ import Ember from "ember";
 import ApplicationRouteMixin from "ember-simple-auth/mixins/application-route-mixin";
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-    setupController: function () {
-        $(window).on('beforeunload', () => {
-            this.get("session").invalidate();
-        });
-    },
     activate() {
         this.controllerFor("application").getRequests();
     }
