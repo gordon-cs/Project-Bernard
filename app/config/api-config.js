8cee5a313c55f2a632ba28840cc65ded7ae00b01
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2016-07-25 10:04:30 -0400

    DRY. Don't Repeate Yourself.

--- app/config/api-config.js
+++ app/config/api-config.js
@@ -1,9 +1,14 @@
 // Configuration file for api host settings
-// Config object
-const Config = {
-  apiUrl: 'https://gordon360api.gordon.edu/api',
-  tokenUrl: 'https://gordon360api.gordon.edu/token'
-};
+
+// Placeholder object because we want to make the config const
+// but a const variable only lets you assign variables in the constructor.
+var placeholder = {};
+placeholder.baseUrl = 'https://gordon360api.gordon.edu';
+placeholder.apiUrl = placeholder.baseUrl + '/api';
+placeholder.tokenUrl = placeholder.baseUrl + '/token';
+
+// Yay const!
+const Config = placeholder;
 
 
 export default Config;
