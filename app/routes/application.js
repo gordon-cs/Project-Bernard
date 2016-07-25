ba70a8d923baf941eb0caadf78ee8df9676b9037
jameskempf <james.kempf@gordon.edu>
2016-07-25 14:31:02 -0400

    force logout on application close

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,11 @@ import Ember from "ember";
 import ApplicationRouteMixin from "ember-simple-auth/mixins/application-route-mixin";
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+    setupController: function () {
+        $(window).on('beforeunload', () => {
+            this.get("session").invalidate();
+        });
+    },
     activate() {
         this.controllerFor("application").getRequests();
     }
