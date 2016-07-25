b21c50718e7d6ee7f2198957e5e443c23bfb63b9
jameskempf <james.kempf@gordon.edu>
2016-07-25 16:12:17 -0400

    styling

--- app/controllers/edit-activity.js
+++ app/controllers/edit-activity.js
@@ -26,20 +26,21 @@ export default Ember.Controller.extend({
 
             /* Image Upload */
             if(this.get("use_default_image")) {
-              let response = postSync('/activities/'+activityCode+'/image/reset',null,this);
+                let response = postSync('/activities/'+activityCode+'/image/reset',null,this);
             }
             else {
-              let image = Ember.$("#file")[0].files[0];
-              let imageValidation = validateImage(image); // See helper method on the bottom
-              if (imageValidation.isValid) {
-                let imageData = new FormData();
-                imageData.append(image.name, image); // Add the image to the FormData object
-                let imageUpload = postFileSync('/activities/'+activityCode+'/image', imageData, this);
-              }
-              else{
-                // TODO alert the user that upload validation failed.
-                console.log(imageValidation.validationMessage + '\nNo image will be uploaded.');
-              }
+                let image = Ember.$("#file")[0].files[0];
+                console.log(image);
+                let imageValidation = validateImage(image); // See helper method on the bottom
+                if (imageValidation.isValid) {
+                    let imageData = new FormData();
+                    imageData.append(image.name, image); // Add the image to the FormData object
+                    let imageUpload = postFileSync('/activities/'+activityCode+'/image', imageData, this);
+                }
+                else{
+                    this.set("errorMessage", imageValidation.validationMessage)
+                    console.log(imageValidation.validationMessage + '\nNo image will be uploaded.');
+                }
             }
             /* End Image Upload */
 
@@ -76,30 +77,30 @@ export default Ember.Controller.extend({
 
 /* HELPER METHODS */
 // Validate the selected image
-function validateImage(file){
-  let validFileExtensions = ['png','jpg','jpeg','bmp','gif'];
-  let result = {
-    isValid : true,
-    validationMessage : ''
-  };
+function validateImage(file) {
+    let validFileExtensions = ['png','jpg','jpeg','bmp','gif'];
+        let result = {
+        isValid : true,
+        validationMessage : ''
+    };
 
-  if(file == undefined) {
-    result.isValid = false;
-    result.validationMessage = "No image file was selected.";
+    if(file == undefined) {
+        result.isValid = false;
+        result.validationMessage = "No image file was selected";
+        return result;
+    }
+
+    let fileExtentsion = file.name.split('.').pop() || '';
+    // The extension is not in the list of valid extensions
+    if(validFileExtensions.indexOf(fileExtentsion) === -1) {
+        result.isValid = false;
+        result.validationMessage = 'Unacceptable file extension';
+    }
+    // File is greater than 100KB
+    if(file.size > 100000) {
+        result.isValid = false;
+        result.validationMessage = 'Unacceptable file size';
+    }
+
     return result;
-  }
-
-  let fileExtentsion = file.name.split('.').pop() || '';
-  // The extension is not in the list of valid extensions
-  if(validFileExtensions.indexOf(fileExtentsion) === -1) {
-    result.isValid = false;
-    result.validationMessage = 'Unacceptable file extension.';
-  }
-  // File is greater than 100KB
-  if(file.size > 100000) {
-    result.isValid = false;
-    result.validationMessage = 'Unacceptable file size.';
-  }
-
-  return result;
 }
