8788b6ad543459f0c97a30e45782bcc99f50ad9c
jameskempf <james.kempf@gordon.edu>
2016-07-26 13:42:21 -0400

    application controller to async

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
 import isLeader from "gordon360/utils/is-leader";
 import getSync from "gordon360/utils/get-sync";
+import getAsync from "gordon360/utils/get-async";
 import deleteSync from "gordon360/utils/delete-sync";
 import sortJsonArray from "gordon360/utils/sort-json-array";
 
@@ -17,83 +18,103 @@ export default Ember.Controller.extend({
         deleteRequest(requestID) {
           let response = deleteSync("/requests/" + requestID, this);
           window.location.reload(true);
-
         }
     },
     // Get requests a user may have to approve or deny
     getRequests() {
-        if ((this.requestsRecieved === null || this.requestsSent === null) &&
-                this.get("session.data.authenticated.token_data")) {
-            // Get recieved requests
-            let leaderMemberships = [];
+        let context = this;
+        let IDNumber = this.get("session.data.authenticated.token_data.id");
+
+        let leaderMemberships = [];
+
+        // Get memberships of user
+        let getMemberships = function() {
+            return getAsync("/memberships/student/" + IDNumber, context);
+        };
+        // Get requests for an activity
+        let getActivityRequests = function() {
+            return getAsync("/requests/activity/" + leaderMemberships[i].ActivityCode, context);
+        };
+        // Get the difference in days bewteen today and specified date
+        // Returns integer and printable string
+        let getDiffDays = function(date) {
+            let currentDate = new Date();
+            let requestDate = new Date(date);
+            let timeDiff = Math.abs(currentDate.getTime() - requestDate.getTime());
+            let diffDays =  Math.floor(timeDiff / (1000 * 3600 * 24));
+            let diffString;
+            if (diffDays === 0) {
+                diffString = "Today";
+            }
+            else if (diffDays === 1) {
+                diffString = "Yesterday";
+            }
+            else {
+                diffString = diffDays.toString() + " days ago";
+            }
+            return {
+                "diffInt": diffDays,
+                "diffString": diffString
+            };
+        }
+        // Chooses requests that pending for the session that the user is a leader of
+        let setRecievedRequests = function(result) {
             let requestsRecieved = [];
-            let response = getSync("/memberships/student/" + this.get("session.data.authenticated.token_data.id"), this);
-            if (response.success) {
-                let memberships = response.data;
-                for (let i = 0; i < memberships.length; i ++) {
-                     if (isLeader(memberships[i].Participation)) {
-                        leaderMemberships.push(memberships[i]);
-                    }
-                }
-                for (let i = 0; i < leaderMemberships.length; i ++) {
-                    // API call util function
-                    let response = getSync("/requests/activity/" + leaderMemberships[i].ActivityCode, this);
-                    if (response.success) {
-                        for (let j = 0; j < response.data.length; j ++) {
-                            if (response.data[j].Session === leaderMemberships[i].Session && response.data[j].RequestApproved === "Pending") {
-                                let diffDays = this.getDiffDays(response.data[j].DateSent);
-                                response.data[j].DiffDays = diffDays.diffString;
-                                response.data[j].DiffDaysInt = diffDays.diffInt;
-                                requestsRecieved.push(response.data[j]);
-                            }
+            for (let i = 0; i < result.length; i ++) {
+                if (result[i].RequestApproved === "Pending") {
+                    for (let j = 0; j < leaderMemberships.length; j ++) {
+                        if (result[i].Session === leaderMemberships[j].Session &&
+                                result[i].ActivityCode === leaderMemberships[j].ActivityCode) {
+                            let diffDays = getDiffDays(result[i].DateSent);
+                            result[i].DiffDays = diffDays.diffString;
+                            result[i].DiffDaysInt = diffDays.diffInt;
+                            requestsRecieved.push(result[i]);
                         }
                     }
                 }
             }
-            // Get sent requests
-            // Only show requests that are younger then 7 days or still pending
-            let requestsSent = getSync("/requests/student/" + this.get("session.data.authenticated.token_data.id"), this).data;
-            for (let i = 0; i < requestsSent.length; i ++) {
-                let diffDays = this.getDiffDays(requestsSent[i].DateSent);
-                requestsSent[i].DiffDays = diffDays.diffString;
-                requestsSent[i].DiffDaysInt = diffDays.diffInt;
-                if (diffDays.diffInt > 7 && requestsSent[i].RequestApproved !== "Pending") {
-                    requestsSent.splice(i, 1);
-                    i --;
+            if (requestsRecieved.length > 0) {
+                context.set("requestsRecieved", sortJsonArray(requestsRecieved, "DiffDaysInt"));
+                context.set("notificationsPresent", true);
+            }
+        };
+        // Chooses requests that are newer then 7 days or still pending
+        let setSentRequests = function(result) {
+            let requestsSent = [];
+            for (let i = 0; i < result.length; i ++) {
+                let diffDays = getDiffDays(result[i].DateSent);
+                result[i].DiffDays = diffDays.diffString;
+                result[i].DiffDaysInt = diffDays.diffInt;
+                if (diffDays.diffInt <= 7 || result[i].RequestApproved === "Pending") {
+                    requestsSent.push(result[i]);
                 }
             }
-            // Set controller variables
-            if (requestsRecieved.length > 0 || requestsSent.length > 0) {
-                this.set("notificationsPresent", true);
-            }
-            if (requestsRecieved.length > 0) {
-                this.set("requestsRecieved", sortJsonArray(requestsRecieved, "DiffDaysInt"));
-            }
             if (requestsSent.length > 0) {
-                this.set("requestsSent", sortJsonArray(requestsSent, "DiffDaysInt"));
+                context.set("requestsSent", sortJsonArray(requestsSent, "DiffDaysInt"));
+                context.set("notificationsPresent", true);
             }
+        };
+        // Removes non-leadership postions and gets all requests for those activities
+        // Gets all requests user has sent
+        let getAllRequests = function(result) {
+            for (let i = 0; i < result.length; i ++) {
+                if (!isLeader(result[i].Participation)) {
+                    result.splice(i --, 1);
+                }
+            }
+            leaderMemberships = result;
+            for (let i = 0; i < leaderMemberships.length; i ++) {
+                getAsync("/requests/activity/" + leaderMemberships[i].ActivityCode, context)
+                .then(setRecievedRequests);
+            }
+            getAsync("/requests/student/" + IDNumber, context)
+            .then(setSentRequests);
+        };
+
+        if ((this.requestsRecieved === null || this.requestsSent === null) &&
+                this.get("session.data.authenticated.token_data")) {
+            getMemberships()
+            .then(getAllRequests)
         }
     },
-    // Get the difference between todays date and given date
-    // Returns JSON object with number of days ago and string to display
-    getDiffDays(date) {
-        let currentDate = new Date();
-        let requestDate = new Date(date);
-        let timeDiff = Math.abs(currentDate.getTime() - requestDate.getTime());
-        let diffDays =  Math.floor(timeDiff / (1000 * 3600 * 24));
-        let diffString;
-        if (diffDays === 0) {
-            diffString = "Today";
-        }
-        else if (diffDays === 1) {
-            diffString = "Yesterday";
-        }
-        else {
-            diffString = diffDays.toString() + " days ago";
-        }
-        return {
-            "diffInt": diffDays,
-            "diffString": diffString
-        };
-    }
 });
