7cb14a19c7bf968746b0a3ec82ec5809ff061e78
Weiqiu You <rachel_you@outlook.com>
2017-05-26 16:58:40 -0400

    Fixed problem with scaling

--- app/components/image-cropper.js
+++ app/components/image-cropper.js
@@ -10,36 +10,18 @@ export default imageCropper.extend({
   cropperContainer: '.cropper-container > img',
   previewClass: '.img-preview',
   croppedAvatar: null,
+  movable: false,
+  zoomable: false,
   imageChange: Ember.observer('image', function() {
      let input = Ember.$('#file')[0];
      let image = this.get('image');
-     console.log(image);
-     console.log(input.files[0]);
      if (input.files && input.files[0]) {
          var reader = new FileReader();
-        //  reader.onload = function (e) {
-        //      $('#image-to-crop').attr('src', e.target.result);
-        //  }
          reader.addEventListener("loadend", function() {
-             $('#image-to-crop').attr('src', reader.result);
-             $('.cropper-canvas > img').attr('src', reader.result);
-             $('.cropper-view-box > img').attr('src', reader.result);
-             $('.img-preview > img').attr('src', reader.result);
+             $('#image-to-crop').cropper('replace',reader.result);
+             $('#cropper-div').show();
          })
          return reader.readAsDataURL(input.files[0]);
      }
-  }),
-  // cropperImageChange: Ember.observer('imageChange', function() {
-  //     $('#image-to-crop').addEventListener("change", function() {
-  //         let file = this.attr('src');
-  //         $('.cropper-canvas > img').attr('src', file);
-  //     });
-  // }),
-  actions: {
-    getCroppedAvatar: function() {
-      var container = this.$(this.get('cropperContainer'));
-      var croppedImage = container.cropper('getCroppedCanvas');
-      this.set('croppedAvatar', croppedImage);
-    }
-  }
+  })
 });
