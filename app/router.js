74dcc3ad7db1bb6fa6b00fdaabe969d73a292f38
Weiqiu You <rachel_you@outlook.com>
2017-06-12 23:01:28 -0400

    Save progress

--- app/router.js
+++ app/router.js
@@ -18,6 +18,7 @@ Router.map(function() {
   this.route('about');
   this.route('help');
   this.route('activities-status');
+  this.route("email", {path: "/email/:Type"});
 });
 
 export default Router;
