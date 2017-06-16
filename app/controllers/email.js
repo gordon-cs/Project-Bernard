7bb3695a0efda6596af447e8c1ef25a028c6916a
Weiqiu You <rachel_you@outlook.com>
2017-06-16 09:57:30 -0400

    Display wrong username or password message. Change to bcc the sender

--- app/controllers/email.js
+++ app/controllers/email.js
@@ -1,5 +1,6 @@
 import Ember from "ember";
 import putAsync from "gordon360/utils/put-async";
+import ENV from "gordon360/config/environment"
 
 /*  Controller for the edit membership page.
  *  Handles user interaction with the page.
@@ -16,23 +17,60 @@ export default Ember.Controller.extend({
     },
     actions: {
         send() {
+            // Make Request for Access Token
+            let makeRequest = function(credentials) {
+                let context = this;
+                var data = {
+                    "username": credentials.username,
+                    "password": credentials.password,
+                    "grant_type": "password"
+                };
+                let makeCall = function() {
+                    return Ember.$.ajax({
+                        type: "POST",
+                        url: ENV['ember-simple-auth-token'].serverTokenEndpoint,
+                        data: data,
+                        dataType: "json"
+                    });
+                }
+                return makeCall();
+            }
+
             let context = this;
             let emailSubject = $("#email-subject").val();
             let emailContent = $("#email-content").html();
+            let username = $("#username").val().replace("@gordon.edu","");
             let password = $("#password").val();
-            let fromID = context.get("session.data.authenticated.token_data.id");
-            let toAddress = context.get("model");
-            let data = {
-                "ToAddress": toAddress,
-                "FromID": fromID,
-                "Subject": emailSubject,
-                "Content": emailContent,
-                "Password": password
+            let credentials = {
+                username: username,
+                password: password
             }
-            putAsync("/emails", data, context).then(function(){
-                $("#email-div").hide();
-                $("#success-msg").show();
-            })
+
+            let sendEmail = function() {
+                let toAddress = context.get("model");
+                let fromAddress = username + "@gordon.edu";
+                let data = {
+                    "ToAddress": toAddress,
+                    "FromAddress": fromAddress,
+                    "Subject": emailSubject,
+                    "Content": emailContent,
+                    "Password": password
+                }
+                putAsync("/emails", data, context)
+                .then(function(){
+                    $("#email-div").hide();
+                    $("#success-msg").show();
+                })
+            }
+
+            let setErrorMsg = function() {
+                context.set("errorMessage","Wrong username or password");
+                $("#password").val("");
+            }
+
+            makeRequest(credentials).then(sendEmail, setErrorMsg);
+
         }
     }
+
 });
