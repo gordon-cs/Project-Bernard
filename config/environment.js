19800f6922724f44791c4c168859d050e4a59911
Sam (Thanh) Nguyen <Sam.Nguyen@gordon.edu>
2017-06-19 14:14:52 -0400

    changed config file back to 360ApiTrain

--- config/environment.js
+++ config/environment.js
@@ -15,12 +15,12 @@ module.exports = function(environment) {
         APP: {
             // Here you can pass flags/options to your application instance
             // when it is created
-            apiUrl: 'http://localhost:4231/api',
-            baseUrl: 'http://localhost:4231'
+            apiUrl: 'https://360ApiTrain.gordon.edu/api',
+            baseUrl: 'https://360ApiTrain.gordon.edu'
 
         },
         'ember-simple-auth-token': {
-            serverTokenEndpoint: 'http://localhost:4231/token',
+            serverTokenEndpoint: 'https://360ApiTrain.gordon.edu/token',
             authorizationPrefix: 'Bearer ',
             authorizationHeaderName: 'Authorization'
         }
