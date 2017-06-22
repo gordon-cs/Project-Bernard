a431d3e05cf1ed4ca74b8ee4288423d413278772
Weiqiu You <rachel_you@outlook.com>
2017-06-22 16:38:54 -0400

    Move My Involvement to a separate page

--- vendor/customize-tab.js
+++ vendor/customize-tab.js
@@ -2,3 +2,8 @@ $('.info-blocks div').hover(function (e) {
   e.preventDefault()
   $(this).tab('show')
 });
+
+$(".my-involvements-page li").click(function() {
+  window.location = $(this).find("a").attr("href");
+  return false;
+});
