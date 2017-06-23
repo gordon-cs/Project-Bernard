0c3a342b9d024df7beb154236f3f3dff80f8c488
Weiqiu You <rachel_you@outlook.com>
2017-06-23 17:00:31 -0400

    Fix error syntax

--- vendor/customize-tab.js
+++ vendor/customize-tab.js
@@ -1,6 +1,4 @@
-import Ember from "ember";
-
-Ember.$('.info-blocks div').hover(function (e) {
+$('.info-blocks div').hover(function (e) {
   e.preventDefault()
   $(this).tab('show')
   console.log("!");
