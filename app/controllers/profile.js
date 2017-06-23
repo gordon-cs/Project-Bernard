3dc54366ddb3eb8fc0489cd5047513c4391379b7
Joe Ross <joeross999@gmail.com>
2017-06-23 13:21:13 -0400

    Progress on uploading profile images

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,5 +1,9 @@
 import Ember from "ember";
 import deleteAsync from "gordon360/utils/delete-async";
+import putAsync from "gordon360/utils/put-async";
+import postFileAsync from "gordon360/utils/post-file-async";
+import postAsync from "gordon360/utils/post-async";
+import imageCropper from 'ember-cli-image-cropper/components/image-cropper';
 
 /*  Controller for the notification table.
  *  Handles user interaction with the page.
@@ -47,7 +51,127 @@ export default Ember.Controller.extend({
               console.log("This is my image");
         },
         cancelEditProfilePicture(){
+            console.log(this.get("file"));
+            this.set("description", null);
+            this.set("pageUrl", null);
+            this.set("file", null);
+            this.set("defaultImage", false);
+            this.set("errorMessage", null);
              $("#toggleEditProfilePictureModal").removeClass("showModal");
-        }
+            console.log(this.get("file"));
+        },
+        update() {
+
+            let context = this;
+
+            // Get the picture submitted by user
+            let image = Ember.$("#file")[0].files[0];
+
+            let error = false;
+
+            // Display error message on the page
+            let showError = function(result) {
+                context.set("errorMessage", new Error(result.responseText));
+            };
+
+            // convert data to blob
+            function dataURItoBlob(dataURI) {
+                // convert base64/URLEncoded data component to raw binary data held in a string
+                var byteString;
+                if (dataURI.split(',')[0].indexOf('base64') >= 0)
+                    byteString = atob(dataURI.split(',')[1]);
+                else
+                    byteString = unescape(dataURI.split(',')[1]);
+
+                // separate out the mime component
+                var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+                // write the bytes of the string to a typed array
+                var ia = new Uint8Array(byteString.length);
+                for (var i = 0; i < byteString.length; i++) {
+                    ia[i] = byteString.charCodeAt(i);
+                }
+
+                return new Blob([ia], {type:mimeString});
+            }
+
+            // Upload image file
+            // Resturns resolved promise if no image was selected
+            let uploadImage = function() {
+                let image = Ember.$("#file")[0].files[0];
+
+                    console.log("uploading image");
+
+                    // let imageValidation = validateImage(image);
+
+                     // See helper method on the bottom
+                        let dataUrl = $("#image-to-crop").cropper('getCroppedCanvas', {
+                            width: 320,
+                            height: 320
+                        }).toDataURL('image/jpeg');
+                        let blob = dataURItoBlob(dataUrl);
+                        let blobName = "canvasImage.jpeg";
+                        console.log(blob);
+                        let imageData = new FormData();
+
+                        imageData.append("canvasImage", blob, "canvasImage.jpeg");
+                        console.log(imageData);
+
+                        return postFileAsync("/profile/" + context.get("model.userInfo.EmailUserName") +
+                          "/image", imageData, context).catch((reason) => {
+                            error = true;
+                            showError(reason);
+                        });
+                    
+            };
+
+            //Check to make sure a valid file was selected
+            let errorChecks = function() {
+                error = false;
+                let passed = true;
+                if(image){
+                    if(image.type === "image/png" || image.type === "image/jpg" || image.type === "image/jpeg" || image.type === "image/bmp" || image.type === "image/gif") {
+                        passed = true;
+                    }else {
+                        passed = false;
+                    }
+                } else {
+                    passed = false;
+                }
+                return passed;
+            };
+
+            // Leave inputs blank and transition back to activity after post
+            let transition = function() {
+                context.set("file", "");
+                context.set("errorMessage", null);
+                transitionModal();
+            };
+
+            let transitionModal = function(){
+                $("#toggleEditProfilePictureModal").removeClass("showModal");
+            }
+
+            let askAgain = function(){
+                let reason = {
+                    responseText: "Please upload one of the supported image types"
+                }
+                showError(reason);
+            };
+            console.log("Starting");
+            if (errorChecks()) {
+                console.log("errorChecks passed");
+                uploadImage()
+                // .then(updateProfile)
+                .then(function() {
+                    console.log("uploaded image");
+                    if (! error) {
+                        transition();
+                    }
+                });
+            } else{
+                askAgain();
+            }
+        },
     }
 });
