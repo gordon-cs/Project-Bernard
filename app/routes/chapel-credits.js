375dfcb73ed783d9ec5a929ac8f68fc67d7ed808
Matthew Felgate <Matthew.Felgate@gordon.edu>
2017-06-26 11:25:26 -0400

    ui testing

--- app/routes/chapel-credits.js
+++ app/routes/chapel-credits.js
@@ -4,85 +4,82 @@ import getAsync from "gordon360/utils/get-async";
 
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model: function(params, transition) {
+    model: function(params, transition) {
 
-    //initialize variables
-    let context = this;
-    let id_number = this.get("session.data.authenticated.token_data.id");
-    let monthArry = ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-
-    //subtract a year if it is the spring semester,
-    //take away the first two digits of the year ie. (yyyy -> yy)
-    //create termcode based on month,and shorten year
-    let month = new Date().getMonth();
-    let date = new Date().getFullYear()-(month >=0&& month <=6? 1:0);
-    let term = (month >=0 && month <=6? "SP": "FA");
-    let subdate = date.toString().substr(-2);
-    let termCode = subdate + term;
+        //initialize variables
+        let context = this;
+        let id_number = this.get("session.data.authenticated.token_data.id");
+        let monthArry = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 
-    console.log(termCode);
-    let load25 = $.ajax('https://25live.collegenet.com/25live/data/gordon/run/events.xml?/&event_id=636&scope=extended');
-    console.log(load25);
-    //retreive the chapel information from the database
-    let loadChapel =  function(){
-      return getAsync("/event/chapel/Student/" + id_number + "/" + termCode , context);};
-
-    //Formate the information you want to display
-    let loadModel = function(result) {
-      console.log(result);
+        //subtract a year if it is the spring semester,
+        //take away the first two digits of the year ie. (yyyy -> yy)
+        //create termcode based on month,and shorten year
+        let month = new Date().getMonth();
+        let date = new Date().getFullYear() - (month >= 0 && month <= 6 ? 1 : 0);
+        let term = (month >= 0 && month <= 6 ? "SP" : "FA");
+        let subdate = date.toString().substr(-2);
+        let termCode = subdate + term;
 
 
-      let chapelEvents = result;
-      let displayTime;
-      let numEvents = chapelEvents.length;
-      let eventsPercent = Math.round((numEvents*100)/30);
+        console.log(termCode);
+        //retreive the chapel information from the database
+        let loadChapel = function() {
+            return getAsync("/event/chapel/Student/" + id_number + "/" + termCode, context);
+        };
 
-      for( let i = 0; i<numEvents; ++i){
+        //Formate the information you want to display
+        let loadModel = function(result) {
+            console.log(result);
 
-        //get the date information
-        let eventDate = new Date(chapelEvents[i].CHDate);
-        let eventMonth = eventDate.getMonth();
-        let eventDay = eventDate.getDate();
-        let eventHour = new Date(chapelEvents[i].CHTime).getHours();
-        let eventMin = new Date(chapelEvents[i].CHTime).getMinutes();
 
-        //insert the formated date back into the JSON array
-        chapelEvents[i].CHDate = monthArry[eventMonth]+ ". "+ eventDay;
+            let chapelEvents = result;
+            let displayTime;
+            let numEvents = chapelEvents.length;
+            let eventsPercent = Math.round((numEvents * 100) / 30);
 
-        console.log(eventMonth);
+            for (let i = 0; i < numEvents; ++i) {
 
-        //create a 12 hour clock
-        if (eventMin < 10){
-          eventMin = "0" + eventMin;
-        }
+                //get the date information
+                let eventDate = new Date(chapelEvents[i].CHDate);
+                let eventMonth = eventDate.getMonth();
+                let eventDay = eventDate.getDate();
+                let eventHour = new Date(chapelEvents[i].CHDate).getHours();
+                let eventMin = new Date(chapelEvents[i].CHDate).getMinutes();
 
-        if (eventHour > 12) {
-          eventHour = eventHour - 12
-          displayTime = eventHour + ":" + eventMin + "pm";
-          }
+                //insert the formated date back into the JSON array
+                chapelEvents[i].CHDate = monthArry[eventMonth] + ". " + eventDay;
 
-       else {
-            displayTime = eventHour + ":" + eventMin + "am";
-          }
+                console.log(eventMonth);
 
-      //insert that back into the JSON array
-      chapelEvents[i].CHTime = displayTime;
+                //create a 12 hour clock
+                if (eventMin < 10) {
+                    eventMin = "0" + eventMin;
+                }
+
+                if (eventHour > 12) {
+                    eventHour = eventHour - 12
+                    displayTime = eventHour + ":" + eventMin + "pm";
+                } else {
+                    displayTime = eventHour + ":" + eventMin + "am";
+                }
+
+                //insert that back into the JSON array
+                chapelEvents[i].CHTime = displayTime;
+            }
+
+            return {
+                //return all the deseired information
+                "chapelEvents": chapelEvents,
+                "numEvents": numEvents,
+                "eventsPercent": eventsPercent
+            };
+        };
+
+        //send to the front end
+        return loadChapel()
+            .then(loadModel)
     }
-
-      return {
-        //return all the deseired information
-        "chapelEvents": chapelEvents,
-        "numEvents":numEvents,
-        "eventsPercent": eventsPercent
-      };
-    };
-
-    //send to the front end
-    return loadChapel()
-      .then(loadModel)
-  }
 
 
 
