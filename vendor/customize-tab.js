a2fdc053db1b801a4e90071523ef2d753f9c8f27
Weiqiu You <rachel_you@outlook.com>
2017-06-27 16:25:26 -0400

    Fix nav bar display weirdness

--- vendor/customize-tab.js
+++ vendor/customize-tab.js
@@ -1,5 +1,6 @@
-$('.info-blocks div').hover(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-  console.log("!");
+Ember.$('.info-blocks .info-block').mouseover(function (e) {
+    console.log("1");
+    e.preventDefault();
+    $(this).tab('show');
+    console.log("!");
 });
